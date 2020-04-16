6ddd4c4d4f58cf1f2971472c57d10df75013a691
Aaron Grant <aaron.b.grant@gmail.com>
2020-04-16 14:09:06 -0500

    Added all functions except for return book.

--- src/Library.java
+++ src/Library.java
@@ -1,14 +1,12 @@
 import java.util.Scanner;
 import static java.lang.Integer.parseInt;
+import java.time.LocalDate;
 
 public class Library {
 
     public static void main(String[] args) {
-        //Establish Scanner
-        var in = new Scanner(System.in);
-
-        //Inventory of books
-        final String[] books = new String [] {
+        //Create arrays to track books in library, if they are loaned, who rented, and dueDates
+        final String[] books = new String[] {
                 "The Musician's Guide to Theory and Analysis",
                 "Concise Introduction to Tonal Harmony",
                 "The Musician's Guide to Fundamentals",
@@ -24,8 +22,11 @@ public class Library {
                 "The Romantic Overture and Musical Form from Rossini to Wagner",
                 "Mozart's Music of Friends"
         };
-	    System.out.println("Welcome to Dr. Grant's Library App!");
-	    System.out.println("");
+        String[] loanedBooks = new String[books.length];
+        String[] renters = new String[books.length];
+        String[] dueDates = new String[books.length];
+
+	    System.out.println("Welcome to Dr. Grant's Library App!\n");
 
 	    boolean notDone = true; //Tracks if the user is done with the program or not.
 
@@ -35,24 +36,85 @@ public class Library {
             System.out.println("  2) List Books on Loan");
             System.out.println("  3) Rent a Book");
             System.out.println("  4) Return a Book");
-            System.out.println("  5) Quit");
-            System.out.println("");
-            var menuChoice = enterOnlyNumInRange("Please make a selection: ", 1, 5);
+            System.out.println("  5) Quit\n");
+
+            var menuChoice = enterOnlyNumInRange("Please make a selection: ", 1, 5,
+                    "Please only enter numbers between 1 and 5");
 
             switch(menuChoice) {
                 case 1:
                     inventory(books);
+                    break;
+                case 2:
+                    bookOnLoan(loanedBooks, renters, books, dueDates);
+                    break;
+                case 3:
+                    rentBook(loanedBooks, renters, books, dueDates);
+                    break;
+                case 4:
+                    returnABook(loanedBooks, renters, books, dueDates);
+                case 5:
+                    System.out.println("\nThanks for using the library!");
+                    System.exit(0);
             }
         }
+    }
 
+    public static void returnABook(String[] loanedBooks, String[] renters, String[] books, String[] dueDates) {
 
+    }
 
+    public static void rentBook(String[] loanedBooks, String[] renters, String[] books, String[] dueDates) {
+        var in = new Scanner(System.in);
+        System.out.println("");
+        String rentedBook = enterOnlyStr("What book will be rented: ", books, "Unknown Book, Please Try Again");
+        System.out.print("Who is renting the book: ");
+        String whoRents = in.nextLine();
+        LocalDate date = LocalDate.now();
+        for (int i = 0; i<books.length; i++) {
+            if (books[i].equalsIgnoreCase(rentedBook)) {
+                loanedBooks[i] = rentedBook;
+                renters[i] = whoRents;
+                dueDates[i] = String.valueOf(date.plusDays(7));
+            }
+        }
+        System.out.println("\nGreat! " + whoRents + " has rented \n" + rentedBook + " until " + String.valueOf(date.plusDays(7)) + ".");
+        backToMenu();
+    }
 
+    public static boolean isEmpty(String[] array) {
+        boolean empty = true;
+        for (int i=0; i<array.length; i++) {
+            if (array[i] != null) {
+                empty = false;
+                break;
+            }
+        }
+        return empty;
+    }
 
+    public static void bookOnLoan(String[] loanedBooks, String[] renters, String[] books, String[] dueDates) {
 
+        //check if array has loaned books
+        if (isEmpty(loanedBooks)) {
+            System.out.println("\nNo books are on loan.");
+            backToMenu();
+        //if not empty, print who has the books
+        } else {
+            findRenter(loanedBooks, renters, books, dueDates);
+            backToMenu();
+        }
+    }
 
-
-
+    public static String findRenter(String[] loanedBooks, String[] renters, String[] books, String[] dueDates) {
+        for (int i = 0; i < loanedBooks.length; i++) {
+            if (loanedBooks[i] != null) {
+                System.out.println(books[i]);
+                System.out.println("Rented by: " + renters[i]);
+                System.out.println("Due on: " + dueDates[i]);
+            }
+        }
+        return null;
     }
 
     public static void inventory(String[] bookList) {
@@ -60,22 +122,16 @@ public class Library {
         for (int i = 0; i < bookList.length; i++) {
             System.out.println((i + 1) + ". " + bookList[i]);
         }
-        System.out.println("");
-        var finished = enterOnlyStr("Would you like to go back to the menu? (y/n) ", "y", "n");
-        if (finished.equals("n")) {
-            System.exit(0);
-        } else {
-            System.out.println("");
-        }
+        backToMenu();
     }
 
-    public static int enterOnlyNumInRange(String message, int smallest, int largest) {
+    public static int enterOnlyNumInRange(String message, int smallest, int largest, String error) {
         var in = new Scanner(System.in);
         System.out.print(message);
         var strNum = in.next();
         if (!isNum(strNum) && parseInt(strNum) >= smallest && parseInt(strNum) <= largest) {
             while (!isNum(strNum) && parseInt(strNum) >= smallest && parseInt(strNum) <= largest) {
-                System.out.println("Incorrect Input, try again");
+                System.out.println(error);
                 System.out.print(message);
                 strNum = in.next();
             }
@@ -85,17 +141,27 @@ public class Library {
         }
     }
 
-    public static String enterOnlyStr(String message, String option1, String option2) {
+    public static String enterOnlyStr(String message, String[] options, String error) {
         var in = new Scanner(System.in);
         System.out.print(message);
-        var str = in.next();
-        if (!str.equals(option1) && !str.equals(option2)) {
-            while (!str.equals(option1) && !str.equals(option2)) {
-                System.out.println("Incorrect Input, try again");
-                System.out.print(message);
-                str = in.next();
+        String str = in.nextLine();
+        boolean isString = false;
+        for (String option : options) {
+            if(str.equalsIgnoreCase(option)) {
+                isString = true;
             }
         }
+        while (!isString) {
+            System.out.println(error);
+            System.out.print(message);
+            str = in.next();
+            for (String option : options) {
+                if(str.equalsIgnoreCase(option)) {
+                    isString = true;
+                }
+            }
+        }
+
         return str;
     }
 
@@ -109,6 +175,18 @@ public class Library {
             return false;
         }
         return true;
+    }
+
+    public static void backToMenu() {
+        System.out.println("");
+        var finished = enterOnlyStr("Would you like to go back to the menu? (y/n) ", new String[] {"y", "n"},
+                "Please only enter y or n");
+        if (finished.equalsIgnoreCase("n")) {
+            System.out.println("\nThanks for using the library!");
+            System.exit(0);
+        } else {
+            System.out.println("");
+        }
     }
 
 }
