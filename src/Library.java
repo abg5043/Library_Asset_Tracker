4a6ab0db65d3d48d727fed68e327146109a103c8
Aaron Grant <aaron.b.grant@gmail.com>
2020-04-16 18:21:02 -0500

    Added feature so that users can add a book to the library

--- src/Library.java
+++ src/Library.java
@@ -7,17 +7,18 @@ import java.time.LocalDate;
 
 public class Library {
 
+    //Password needed for changing the books in the library
+    final static String PASSWORD = "TEST";
+
     /** The following method contains the main menu features of the class. It also contains the current library books.
-     * Later features of this method could include additional menu options to add and subtract books from the library
-     * if a user had a given password.
      *
-     * @input the user interacts with the menu through standard input. Users need only enter integers.
+     * @input the user interacts with the menu through standard input. Users can only enter integers.
      *
      */
 
     public static void main(String[] args) {
         //Create arrays to track books in library, if they are loaned, who rented, and dueDates
-        final String[] books = new String[] {
+        String[] books = new String[] {
                 "The Musician's Guide to Theory and Analysis",
                 "Concise Introduction to Tonal Harmony",
                 "The Musician's Guide to Fundamentals",
@@ -33,6 +34,7 @@ public class Library {
                 "The Romantic Overture and Musical Form from Rossini to Wagner",
                 "Mozart's Music of Friends"
         };
+
         String[] loanedBooks = new String[books.length];
         String[] renters = new String[books.length];
         String[] dueDates = new String[books.length];
@@ -47,9 +49,10 @@ public class Library {
             System.out.println("  2) List Books on Loan");
             System.out.println("  3) Rent a Book");
             System.out.println("  4) Return a Book");
-            System.out.println("  5) Quit\n");
+            System.out.println("  5) Add a Book (only if you have the password)");
+            System.out.println("  6) Quit\n");
 
-            var menuChoice = enterOnlyNumInRange("Please make a selection: ", 1, 5);
+            var menuChoice = enterOnlyNumInRange("Please make a selection: ", 1, 6);
 
             switch(menuChoice) {
                 case 1:
@@ -65,10 +68,81 @@ public class Library {
                     returnABook(loanedBooks, renters, books, dueDates);
                     break;
                 case 5:
+                    renters = addAnEntry(renters);
+                    loanedBooks = addAnEntry(loanedBooks);
+                    dueDates = addAnEntry(dueDates);
+                    books = addABook(books);
+                    break;
+                case 6:
                     System.out.println("\nThanks for using the library!");
                     System.exit(0);
             }
         }
+    }
+
+    /** The following method adds a null entry to a given array, but keeps all current entries at their current indexes
+     *
+     * @param array = any array
+     * @return = a copied array of array.length + 1;
+     */
+
+
+    public static String[] addAnEntry(String[] array) {
+        String[] newArr = new String[array.length + 1];
+        for (int i = 0; i < array.length; i++) {
+            newArr[i] = array[i];
+        }
+        return newArr;
+    }
+
+    /** The following method adds a book to the library's collection if the user has the password
+     *
+     * @param books = an array of all books the library owns
+     * @input the user enters the new book through standard input.
+     * @return = the book array either modified or not depending on if user had password
+     */
+
+    public static String[] addABook(String[] books) {
+        var in = new Scanner(System.in);
+        //check to see if user has the correct password or not
+        if (askForPass()) {
+            System.out.println("\nThanks for the correct password!\nPlease enter the new book: ");
+            String newBook = in.nextLine();
+
+            //add an entry to the book array
+            books = addAnEntry(books);
+
+            //add the new book
+            books[books.length-1] = newBook;
+            System.out.println("");
+            return books;
+
+        } else {
+            System.out.println("\nSorry! You ran out of attempts. Taking you back to the main menu.\n");
+            return books;
+        }
+    }
+
+    /** The following method prompts the user for the password and gives them 3 attempts.
+     *
+     * @input the user enters the password through standard input.
+     * @return = boolean true or false
+     */
+
+    public static boolean askForPass() {
+        var in = new Scanner(System.in);
+        int attempts = 3;
+        String password = "";
+        while (attempts > 0 && !PASSWORD.equals(password)) {
+            attempts--;
+            System.out.print("Enter your password: ");
+            password = in.nextLine();
+            if (password.equals(PASSWORD))
+                return true;
+            else
+                System.out.println("Incorrect. Number of attempts remaining: " + attempts);
+        }
+        return false;
     }
 
     /** The following method returns a book to the library and removes the loaned out book information from the
@@ -86,7 +160,7 @@ public class Library {
         System.out.println("");
 
         //User inputs book; program checks if it is a loaned out book or not
-        String rentedBook = enterOnlyStr("What book will be rented: ", loanedBooks, "Rented book not found. Please Try Again");
+        String rentedBook = enterOnlyGivenStr("What book will be rented: ", loanedBooks, "Rented book not found. Please Try Again");
         String formattedRentedBook = "";
 
         //remove loaned book info from associated arrays including due dates, renter, and loaned book
@@ -118,21 +192,21 @@ public class Library {
         System.out.println("");
 
         //User inputs book; program checks if it is in the library or not
-        String rentedBook = enterOnlyStr("What book will be rented: ", books, "Unknown Book, Please Try Again");
+        String rentedBook = enterOnlyGivenStr("What book will be rented: ", books, "Unknown Book, Please Try Again");
         int i = 0;
         while (i<books.length) {
 
         //check if book is already rented
             if (rentedBook.equalsIgnoreCase(loanedBooks[i])) {
                 System.out.println("");
-                var finished = enterOnlyStr("Sorry, that book is rented until " + dueDates[i] + ".\nWould you like to try again? (y/n) ",
+                var finished = enterOnlyGivenStr("Sorry, that book is rented until " + dueDates[i] + ".\nWould you like to try again? (y/n) ",
                         new String[] {"y", "n"},"Please only enter y or n");
                 if (finished.equalsIgnoreCase("n")) {
                     System.out.println("");
                     return;
                 } else {
                     System.out.println("");
-                    rentedBook = enterOnlyStr("What book will be rented: ", books, "Unknown Book, Please Try Again");
+                    rentedBook = enterOnlyGivenStr("What book will be rented: ", books, "Unknown Book, Please Try Again");
                     i = 0;
                 }
             }
@@ -283,7 +357,7 @@ public class Library {
      * @return a valid string
      */
 
-    public static String enterOnlyStr(String message, String[] options, String error) {
+    public static String enterOnlyGivenStr(String message, String[] options, String error) {
         var in = new Scanner(System.in);
 
         //ask user for a specific type of string
@@ -299,6 +373,7 @@ public class Library {
         }
 
         //if not in options, keep asking user for a string that is included in options
+        int count = 0;
         while (!isString) {
             System.out.println(error);
             System.out.print(message);
@@ -346,7 +421,7 @@ public class Library {
 
     public static void backToMenu() {
         System.out.println("");
-        var finished = enterOnlyStr("Would you like to go back to the menu? (y/n) ", new String[] {"y", "n"},
+        var finished = enterOnlyGivenStr("Would you like to go back to the menu? (y/n) ", new String[] {"y", "n"},
                 "Please only enter y or n");
         if (finished.equalsIgnoreCase("n")) {
             System.out.println("\nThanks for using the library!");
