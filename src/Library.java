4f9d38d64ebc02e18e66681751e6baa515445af3
Aaron Grant <aaron.b.grant@gmail.com>
2020-04-16 10:23:47 -0500

    Added book inventory

--- src/Library.java
+++ src/Library.java
@@ -1,6 +1,36 @@
+import java.util.Scanner;
+import static java.lang.Integer.parseInt;
+
 public class Library {
 
     public static void main(String[] args) {
-	// write your code here
+        //Inventory of books
+        final String[] books = new String [] {
+                "The Musician's Guide to Theory and Analysis",
+                "Concise Introduction to Tonal Harmony",
+                "The Musician's Guide to Fundamentals",
+                "Manual for Ear Training and Sight Singing",
+                "A Geometry of Music",
+                "Audacious Euphony",
+                "In the Process of Becoming",
+                "Mahler's Symphonic Sonatas",
+                "Contemporary Musicianship: Analysis and the Artist",
+                "Pieces of Tradition",
+                "Tonality and Transformation",
+                "The Cambridge History of Western Music Theory",
+                "The Romantic Overture and Musical Form from Rossini to Wagner",
+                "Mozart's Music of Friends"
+        };
+	    System.out.println("Welcome to Dr. Grant's Library App!");
+	    System.out.println("");
+
+
+
+
+
+
+
+
+
     }
 }
