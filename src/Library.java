29336cac0c9ffbf5bdf321aaadad85944cf4ed0c
Aaron Grant <aaron.b.grant@gmail.com>
2020-04-16 16:06:53 -0500

    Added Javadoc comments

--- src/Library.java
+++ src/Library.java
@@ -1,7 +1,19 @@
 import java.util.Scanner;
 import static java.lang.Integer.parseInt;
 import java.time.LocalDate;
+
+/** This class keeps track of books {@code books} on loan in a given library.
+ */
+
 public class Library {
+
+    /** The following method contains the main menu features of the class. It also contains the current library books.
+     * Later features of this method could include additional menu options to add and subtract books from the library
+     * if a user had a given password.
+     *
+     * @input the user interacts with the menu through standard input. Users need only enter integers.
+     *
+     */
 
     public static void main(String[] args) {
         //Create arrays to track books in library, if they are loaned, who rented, and dueDates
@@ -59,11 +71,25 @@ public class Library {
         }
     }
 
+    /** The following method returns a book to the library and removes the loaned out book information from the
+     * loaned book array {@code loanedBooks}, the renters array {@code renters}, and the due date array {@code dueDates}.
+     *
+     * @param loanedBooks = an array of all books checked out from the library
+     * @param renters = an array of all people renting books
+     * @param books = an array of all books the library owns
+     * @param dueDates = an array of all due dates for all rented books
+     * @input the user enters the book through standard input. Books not in rented array will not be accepted.
+     */
+
     public static void returnABook(String[] loanedBooks, String[] renters, String[] books, String[] dueDates) {
         var in = new Scanner(System.in);
         System.out.println("");
+
+        //User inputs book; program checks if it is a loaned out book or not
         String rentedBook = enterOnlyStr("What book will be rented: ", loanedBooks, "Rented book not found. Please Try Again");
         String formattedRentedBook = "";
+
+        //remove loaned book info from associated arrays including due dates, renter, and loaned book
         for (int i = 0; i<books.length; i++) {
             if (books[i].equalsIgnoreCase(rentedBook)) {
                 formattedRentedBook = books[i];
@@ -76,14 +102,27 @@ public class Library {
         backToMenu();
     }
 
+    /** The following method rents a book from the library and adds the loaned out book information to the
+     * loaned book array {@code loanedBooks}, the renters array {@code renters}, and the due date array {@code dueDates}.
+     *
+     * @param loanedBooks = an array of all books checked out from the library
+     * @param renters = an array of all people renting books
+     * @param books = an array of all books the library owns
+     * @param dueDates = an array of all due dates for all rented books
+     * @input the user enters the book through standard input. Books not in library will not be accepted. Neither will
+     * books that are already rented.
+     */
+
     public static void rentBook(String[] loanedBooks, String[] renters, String[] books, String[] dueDates) {
         var in = new Scanner(System.in);
         System.out.println("");
-        String rentedBook = enterOnlyStr("What book will be rented: ", books, "Unknown Book, Please Try Again");
 
-        //check if book is already rented
+        //User inputs book; program checks if it is in the library or not
+        String rentedBook = enterOnlyStr("What book will be rented: ", books, "Unknown Book, Please Try Again");
         int i = 0;
         while (i<books.length) {
+
+        //check if book is already rented
             if (rentedBook.equalsIgnoreCase(loanedBooks[i])) {
                 System.out.println("");
                 var finished = enterOnlyStr("Sorry, that book is rented until " + dueDates[i] + ".\nWould you like to try again? (y/n) ",
@@ -100,12 +139,13 @@ public class Library {
             i++;
         }
 
-        //if not, rent it out!
+        //if not already rented, then rent it out!
         System.out.print("Who is renting the book: ");
         String whoRents = in.nextLine();
         LocalDate date = LocalDate.now();
         String formattedRentedBook = "";
 
+        //adds rented info to the associated arrays
         for (i = 0; i<books.length; i++) {
             if (books[i].equalsIgnoreCase(rentedBook)) {
                 formattedRentedBook = books[i];
@@ -118,6 +158,11 @@ public class Library {
         backToMenu();
     }
 
+    /** The following method checks if an array has any values or is just filled will null.
+     *
+     * @param array = any string array
+     * @return = a boolean true or false
+     */
     public static boolean isEmpty(String[] array) {
         boolean empty = true;
         for (int i=0; i<array.length; i++) {
@@ -128,6 +173,16 @@ public class Library {
         }
         return empty;
     }
+
+
+    /** The following method checks to see if any books are on loan and prints out any books that currently are along with
+     * the information about who is renting the book and when the book is due.
+     *
+     * @param loanedBooks = an array of all books checked out from the library
+     * @param renters = an array of all people renting books
+     * @param books = an array of all books the library owns
+     * @param dueDates = an array of all due dates for all rented books
+     */
 
     public static void bookOnLoan(String[] loanedBooks, String[] renters, String[] books, String[] dueDates) {
 
@@ -142,7 +197,16 @@ public class Library {
         }
     }
 
-    public static String findRenter(String[] loanedBooks, String[] renters, String[] books, String[] dueDates) {
+    /** The following method finds and prints out any books that are currently being rented along with the info about who
+     * is renting the book and when it is due.
+     *
+     * @param loanedBooks = an array of all books checked out from the library
+     * @param renters = an array of all people renting books
+     * @param books = an array of all books the library owns
+     * @param dueDates = an array of all due dates for all rented books
+     */
+
+    public static void findRenter(String[] loanedBooks, String[] renters, String[] books, String[] dueDates) {
 
         for (int i = 0; i < loanedBooks.length; i++) {
             if (loanedBooks[i] != null) {
@@ -152,8 +216,12 @@ public class Library {
                 System.out.println("Due on: " + dueDates[i]);
             }
         }
-        return null;
     }
+
+    /** The following method finds and prints what books are currently owned by the library.
+     *
+     * @param bookList = an array of all books the library owns.
+     */
 
     public static void inventory(String[] bookList) {
         System.out.println("");
@@ -163,42 +231,74 @@ public class Library {
         backToMenu();
     }
 
+    /** The following method checks if a number is a number and if it falls between two numbers of a given range.
+     *
+     * @param message = a message printed to the user requesting a specific type of number
+     * @param smallest = the smallest number possible to be inputted by the user
+     * @param largest = the largest number possible to be inputted by the user
+     *
+     * @input input an int from standard input. Only integers that fall between the range will be accepted
+     * @return a valid integer
+     */
+
     public static int enterOnlyNumInRange(String message, int smallest, int largest) {
         var in = new Scanner(System.in);
+
+        //ask the user for a type of number
         System.out.print(message);
         var strNum = in.nextLine();
-        if (!isNum(strNum)) {
+
+        //check if that input really is a number or doesn't fall between the range
+        while (!isNum(strNum) || parseInt(strNum) < smallest || parseInt(strNum) > largest) {
+
+            //first check: if it's not a number, keep asking until the user inputs one
             while (!isNum(strNum)) {
                 System.out.println("Not a number, please try again");
                 System.out.print(message);
                 strNum = in.nextLine();
             }
-            while (parseInt(strNum) < smallest || parseInt(strNum) > largest) {
+
+            //second check: is the number within the range?
+            if (parseInt(strNum) < smallest || parseInt(strNum) > largest) {
                 System.out.println("Please only enter numbers between " + smallest + " and " + largest);
                 System.out.print(message);
                 strNum = in.nextLine();
+            } else {
+                return parseInt(strNum);
             }
-            return parseInt(strNum);
-        } else {
-            while (parseInt(strNum) < smallest || parseInt(strNum) > largest) {
-                System.out.println("Please only enter numbers between " + smallest + " and " + largest);
-                System.out.print(message);
-                strNum = in.nextLine();
-            }
-            return parseInt(strNum);
         }
+
+        //return straight out if it passes both tests
+        return parseInt(strNum);
     }
+
+
+    /** The following method checks if a user-inputted string matches strings from a given string array {@code options}.
+     *
+     * @param message = a message printed to the user requesting a specific type of string
+     * @param options = an array of strings that the user-inputted string must match (regardless of case)
+     * @param error = an error message telling the user what to input if they were wrong
+     *
+     * @input input a string from standard input. Only integers that match {@code options} will be accepted
+     * @return a valid string
+     */
 
     public static String enterOnlyStr(String message, String[] options, String error) {
         var in = new Scanner(System.in);
+
+        //ask user for a specific type of string
         System.out.print(message);
         String str = in.nextLine();
         boolean isString = false;
+
+        //check if the string matches any strings in the options array
         for (String option : options) {
             if(str.equalsIgnoreCase(option)) {
                 isString = true;
             }
         }
+
+        //if not in options, keep asking user for a string that is included in options
         while (!isString) {
             System.out.println(error);
             System.out.print(message);
@@ -210,20 +310,39 @@ public class Library {
             }
         }
 
+        //return valid string
         return str;
     }
 
+
+    /** The following method checks if an integer is truly an integer.
+     *
+     * @param strNum = a test string
+     *
+     * @return a boolean value
+     */
+
     public static boolean isNum(String strNum) {
+        //if it's a null string, it obviously isn't a number
         if (strNum == null) {
             return false;
         }
+
+        //try to call Integer.parseInt
         try {
             int d = Integer.parseInt(strNum);
+        //if a number format exception error happens, return false; it's not an integer
         } catch (NumberFormatException nfe) {
             return false;
         }
+        //if that error does not happen, return true; it is an integer.
         return true;
     }
+
+    /** The following method checks if the user wants to go back to the main menu.
+     *
+     * @input = the program checks with standard input to see if the user wants to quit the program or go back to the main menu.
+     */
 
     public static void backToMenu() {
         System.out.println("");
